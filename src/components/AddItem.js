5acd880adc7757c989378340ab7f21f033b597a0
daressel <nevaliashkaoxxxy@gmail.com>
2021-12-19 17:00:07 +0300

    reinstall lock file add semicolons

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,22 +1,20 @@
-import { useState } from "react"
-import { Input } from "antd"
+import { useState } from "react";
+import { Input } from "antd";
 
 export const AddItem = ({ handleAddItem }) => {
-  const [itemName, setItemName] = useState("")
+  const [itemName, setItemName] = useState("");
 
   // this handler has only 1 string
   // but created this for perhaps change in the future
   const changeText = (e) => {
-    setItemName(e.target.value)
-  }
+    setItemName(e.target.value);
+  };
 
   const handlePressEnter = (e) => {
-    e.preventDefault()
-    handleAddItem(e.target.value)
-    setItemName("")
-  }
+    e.preventDefault();
+    handleAddItem(e.target.value);
+    setItemName("");
+  };
 
-  return (
-    <Input value={itemName} onChange={changeText} onPressEnter={handlePressEnter}></Input>
-  )
-}
+  return <Input value={itemName} onChange={changeText} onPressEnter={handlePressEnter}></Input>;
+};
